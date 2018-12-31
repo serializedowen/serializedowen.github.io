985722549bc4a3d2cb890fae9627903909342464
serializedowen <jiah.wang@mail.utoronto.ca>
2018-12-31 01:28:55 -0500

    layout fix and new post

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,25 +4,11 @@ import React from 'react';
 import Helmet from 'react-helmet';
 import { Link } from 'gatsby';
 import styled from 'styled-components';
-import { Layout, Wrapper, Header, Button } from 'components';
-import { media } from '../utils/media';
+import { Layout, Wrapper, Header, Button, Content as Con } from 'components';
 
 import config from '../../config/SiteConfig';
 
-const Content = styled.div`
-  grid-column: 2;
-  box-shadow: 0 4px 120px rgba(0, 0, 0, 0.1);
-  border-radius: 1rem;
-  padding: 2rem 4rem;
-  background-color: ${props => props.theme.colors.bg};
-  z-index: 9000;
-  margin-top: -3rem;
-  @media ${media.tablet} {
-    padding: 3rem 3rem;
-  }
-  @media ${media.phone} {
-    padding: 2rem 1.5rem;
-  }
+const Content = styled(Con)`
   form {
     p {
       label,
@@ -30,7 +16,7 @@ const Content = styled.div`
         display: block;
       }
       input {
-        min-width: 275px;
+        min-width: 230px;
         margin-top: 0.5rem;
       }
       textarea {
