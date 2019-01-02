1835146b0d7a59d20ce705e07a8fb6d7fa8c1af7
serializedowen <jiah.wang@mail.utoronto.ca>
2019-01-02 09:05:58 -0500

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-3c206162926c72fafa71.js"
+    "url": "webpack-runtime-bb39ac09c76c2919e4cb.js"
   },
   {
     "url": "app-54d9723ad10d15c4fd68.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "ae3da8ee8b3c41c44d34bd65cd708e77"
+    "revision": "41b76120e55d13c1aa5e5045548a7128"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json"
