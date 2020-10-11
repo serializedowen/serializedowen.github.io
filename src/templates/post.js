efa1b30fd4f1a60beacd8d30e8df177ba15b55bf
serializedowen <wjh199455@gmail.com>
2020-10-11 23:50:22 +0800

    first attempt on comments

--- src/templates/post.js
+++ src/templates/post.js
@@ -21,7 +21,7 @@ import Card from '@material-ui/core/Card'
 import CardHeader from '@material-ui/core/CardHeader'
 import CardContent from '@material-ui/core/CardContent'
 import Avatar from '@material-ui/core/Avatar'
-
+import useIdentifier from 'src/hooks/useIdentifier'
 import axios from 'src/utils/http'
 
 const Title = styled.h1`
@@ -35,6 +35,27 @@ const PostContent = styled.div`
 
 const Post = props => {
   const [progress, setprogress] = useState(0)
+  const identifier = useIdentifier()
+
+  const [comments, setcomments] = useState([
+    {
+      id: 38,
+      userId: 7,
+      content: 'rssrsr',
+      identifier: 'aaa',
+      createdAt: '2020-10-10T05:49:34.000Z',
+      updatedAt: '2020-10-10T05:49:34.000Z',
+      user_id: 7
+    }
+  ])
+
+  useEffect(() => {
+    axios.get('/comments/' + identifier).then(res => {
+      const { data } = res
+
+      setcomments(data)
+    })
+  }, [])
 
   useEffect(() => {
     let handler
@@ -83,109 +104,26 @@ const Post = props => {
         />
         <PrevNext prev={prev} next={next} />
         <div>
-          <Card>
-            {/* <CardContent>stub comment</CardContent> */}
-            <CardHeader
-              avatar={
-                <Avatar
-                  aria-label="avatar"
-                  src="https://avatars3.githubusercontent.com/u/9219215?v=4"
-                ></Avatar>
-              }
-              title={'serializedowen'}
-            ></CardHeader>
-          </Card>
+          {comments.map(comment => (
+            <Card>
+              <CardContent>{comment.content}</CardContent>
+              <CardHeader
+                avatar={
+                  <Avatar
+                    aria-label="avatar"
+                    src="https://avatars3.githubusercontent.com/u/9219215?v=4"
+                  ></Avatar>
+                }
+                title={'serializedowen'}
+                subheader={comment.createdAt}
+              ></CardHeader>
+            </Card>
+          ))}
         </div>
       </Content>
     </Wrapper>
   )
 }
-
-// class Post extends Component {
-//   constructor(props) {
-//     super(props)
-//     this.state = { progress: 0, comments: [] }
-//   }
-
-//   componentDidMount() {
-//     // using import cause failure at production build
-//     /* eslint global-require:0 */
-//     // const MediumEditor = require('medium-editor')
-//     // this.editor = new MediumEditor('.editable', {
-//     //   elementsContainer: document.querySelector('#medium-toolbar')
-//     // })
-//     console.log(this.props)
-
-//     axios.get('/comments/').then(res => {})
-
-//     setTimeout(() => {
-//       const height = document.body.clientHeight
-//       const { innerHeight } = window
-
-//       console.log(height, innerHeight)
-//       func = () => {
-//         this.setState({
-//           progress: Number((window.scrollY * 100) / (height - innerHeight))
-//         })
-//       }
-//       window.addEventListener('scroll', func)
-//     }, 500)
-//   }
-
-//   componentWillUnmount() {
-//     this.editor = null
-//     func && window.removeEventListener('scroll', func)
-//   }
-
-//   render() {
-//     const {
-//       pageContext: { slug, prev, next },
-//       data: { markdownRemark: postNode }
-//     } = this.props
-
-//     const { progress } = this.state
-//     const post = postNode.frontmatter
-//     return (
-//       <Wrapper>
-//         <ProgressBar progress={progress}></ProgressBar>
-//         <SEO postPath={slug} postNode={postNode} postSEO />
-//         <Helmet title={`${post.title} | ${config.siteTitle}`} />
-//         <Header>
-//           <Link to="/">{config.siteTitle}</Link>
-//         </Header>
-//         <Content>
-//           <Title>{post.title}</Title>
-//           <div id="medium-toolbar" />
-//           <Subline>
-//             {post.date} &mdash; {postNode.timeToRead} Min Read &mdash; In{' '}
-//             <Link to={`/categories/${kebabCase(post.category)}`}>
-//               {post.category}
-//             </Link>
-//           </Subline>
-//           <PostContent
-//             className="editable"
-//             dangerouslySetInnerHTML={{ __html: postNode.html }}
-//           />
-//           <PrevNext prev={prev} next={next} />
-//           <div>
-//             <Card>
-//               {/* <CardContent>stub comment</CardContent> */}
-//               <CardHeader
-//                 avatar={
-//                   <Avatar
-//                     aria-label="avatar"
-//                     src="https://avatars3.githubusercontent.com/u/9219215?v=4"
-//                   ></Avatar>
-//                 }
-//                 title={'serializedowen'}
-//               ></CardHeader>
-//             </Card>
-//           </div>
-//         </Content>
-//       </Wrapper>
-//     )
-//   }
-// }
 
 export { Post }
 export default withENLayout(Post)
--- src/utils/http.js
+++ src/utils/http.js
@@ -3,5 +3,7 @@ import axios from 'axios'
 const isProd = process.env.NODE_ENV === 'production'
 
 export default axios.create({
-  baseURL: isProd ? 'gateway.serializedowen.com' : 'localhost:7001'
+  baseURL: isProd
+    ? 'http://gateway.serializedowen.com'
+    : 'http://localhost:7001'
 })
