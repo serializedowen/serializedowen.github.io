36241e8bf5025575d00c278c8a226b33a0f72e0c
serializedowen22 <wjh199455@gmail.com>
2021-01-07 22:38:23 +0800

    Updates

---
+++ component---src-pages-index-zh-js-49fc06e1f6765c854ead.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[21],{NIjN:function(e,n,a){"use strict";a.r(n);var c=a("RXBc"),o=a("kPEo");n.default=Object(o.a)(c.IndexPage)}}]);
+//# sourceMappingURL=component---src-pages-index-zh-js-49fc06e1f6765c854ead.js.map
--- component---src-pages-index-zh-js-bf811a2bc44edbce6297.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[20],{NIjN:function(e,n,a){"use strict";a.r(n);var c=a("RXBc"),o=a("kPEo");n.default=Object(o.a)(c.IndexPage)}}]);
-//# sourceMappingURL=component---src-pages-index-zh-js-bf811a2bc44edbce6297.js.map
